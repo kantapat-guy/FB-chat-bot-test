097cc9e90a819c2c64a7f94aea6ae2ff4a5cf13d
kantapat-guy <107095121+kantapat-guy@users.noreply.github.com>
2023-05-31 21:36:16 +0700

    test again

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -63,12 +63,12 @@ const callSendAPI = async (sender_psid, response) => {
   }
 
   try {
-    await axios.post(
+    const request = await axios.post(
       `https://graph.facebook.com/v17.0/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`,
       request_body,
       { headers: { "content-type": "application/json" } }
     )
-    console.log("message sent!!")
+    console.log("message sent!! ", request)
   } catch (err) {
     console.error("Unable to send message: ", err)
   }
