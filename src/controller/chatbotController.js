5aacb41128cf8eae02ea032a88fbe094034e6bae
kantapat-guy <107095121+kantapat-guy@users.noreply.github.com>
2023-06-01 03:51:35 +0700

    213132

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -7,60 +7,6 @@ const test = (req, res) => {
   return res.send("Hello again")
 }
 
-const getWebHook = (req, res) => {
-  // Parse the query params
-  let mode = req.query["hub.mode"]
-  let token = req.query["hub.verify_token"]
-  let challenge = req.query["hub.challenge"]
-
-  // Check if a token and mode is in the query string of the request
-  if (mode && token) {
-    // Check the mode and token sent is correct
-    if (mode === "subscribe" && token === process.env.MY_VERIFY_TOKEN) {
-      // Respond with the challenge token from the request
-      console.log("WEBHOOK_VERIFIED")
-      res.status(200).send(challenge)
-    } else {
-      // Respond with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403)
-    }
-  }
-}
-
-const postWebHook = (req, res) => {
-  let body = req.body
-
-  console.log(`\u{1F7EA} Received webhook:`)
-  console.dir(body, { depth: null })
-
-  // Check if this is an event from a page subscription
-  if (body.object === "page") {
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED")
-
-    body.entry.forEach(async function (entry) {
-      // let webhook_event = entry.messaging[0]
-      // console.log(webhook_event)
-
-      // Iterate over webhook events - there may be multiple
-      entry.messaging.forEach(async function (webhookEvent) {
-        let sender_psid = webhookEvent.sender.id
-        console.log("Sender ID:", sender_psid)
-  
-        if (webhookEvent.message) {
-          console.log("trigger event......")
-          handleMessage(sender_psid, webhookEvent.message)
-        } else if (webhookEvent.postback) {
-          handlePostBack(sender_psid, webhookEvent.message)
-        }
-      })
-
-    })
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404)
-  }
-}
 
 const handleMessage = (sender_psid, receive_message) => {
   console.log("handle message......")
@@ -121,6 +67,62 @@ const callSendAPI = async (sender_psid, res_user) => {
 const handlePostBack = () => {
   console.log("postBack")
 }
+
+const getWebHook = (req, res) => {
+  // Parse the query params
+  let mode = req.query["hub.mode"]
+  let token = req.query["hub.verify_token"]
+  let challenge = req.query["hub.challenge"]
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === process.env.MY_VERIFY_TOKEN) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED")
+      res.status(200).send(challenge)
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403)
+    }
+  }
+}
+
+const postWebHook = (req, res) => {
+  let body = req.body
+
+  console.log(`\u{1F7EA} Received webhook:`)
+  console.dir(body, { depth: null })
+
+  // Check if this is an event from a page subscription
+  if (body.object === "page") {
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED")
+
+    body.entry.forEach(async function (entry) {
+      // let webhook_event = entry.messaging[0]
+      // console.log(webhook_event)
+
+      // Iterate over webhook events - there may be multiple
+      entry.messaging.forEach(async function (webhookEvent) {
+        let sender_psid = webhookEvent.sender.id
+        console.log("Sender ID:", sender_psid)
+  
+        if (webhookEvent.message) {
+          console.log("trigger event......")
+          handleMessage(sender_psid, webhookEvent.message)
+        } else if (webhookEvent.postback) {
+          handlePostBack(sender_psid, webhookEvent.message)
+        }
+      })
+
+    })
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404)
+  }
+}
+
 
 module.exports = {
   test: test,
