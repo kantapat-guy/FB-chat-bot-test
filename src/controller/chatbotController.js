dabfe5d6d3d216b2076728a35c6d17cdd5d4e1d2
kantapat-guy <107095121+kantapat-guy@users.noreply.github.com>
2023-06-01 02:23:22 +0700

    fix hook link

--- src/controller/chatbotController.js
+++ src/controller/chatbotController.js
@@ -28,8 +28,14 @@ const getWebHook = (req, res) => {
 const postWebHook = (req, res) => {
   let body = req.body
 
+  console.log(`\u{1F7EA} Received webhook:`)
+  console.dir(body, { depth: null })
+
   // Check if this is an event from a page subscription
   if (body.object === "page") {
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED")
+
     body.entry.forEach(function (entry) {
       let webhook_event = entry.messaging[0]
       console.log(webhook_event)
@@ -40,43 +46,58 @@ const postWebHook = (req, res) => {
       if (webhook_event.message) {
         console.log("trigger event......")
         handleMessage(sender_psid, webhook_event.message)
+      } else if (webhook_event.postback) {
+        handlePostBack(sender_psid, webhook_event.message)
       }
-      //   else if (webhook_event.postback) {
-      //     handlePostBack(sender_psid, webhook_event.message)
-      //   }
     })
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED")
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404)
   }
 }
 
-const callSendAPI = async (sender_psid, response) => {
+const callSendAPI = async (sender_psid, res_user) => {
   let request_body = {
     recipient: {
       id: sender_psid,
     },
-    message: response,
+    message: res_user,
+  }
+  // const url = `https://graph.facebook.com/v17.0/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`
+
+  let url = new URL(`https://graph.facebook.com/v17.0/me/messages`)
+  url.search = new URLSearchParams({
+    access_token: process.env.PAGE_ACCESS_TOKEN,
+  })
+
+  let response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(request_body),
+  })
+
+  if (!response.ok) {
+    console.warn(
+      `Unable to call Send API: ${response.statusText}`,
+      await response.json()
+    )
   }
 
-  try {
-    const request = await axios.post(
-      `https://graph.facebook.com/v17.0/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`,
-      request_body,
-      { headers: { "content-type": "application/json" } }
-    )
-    console.log("message sent!! ", request)
-  } catch (err) {
-    console.error("Unable to send message: ", err)
-  }
+  // try {
+  //   const request = await axios.post(
+  //     `https://graph.facebook.com/v17.0/me/messages?access_token=${process.env.PAGE_ACCESS_TOKEN}`,
+  //     request_body,
+  //     { headers: { "content-type": "application/json" } }
+  //   )
+  //   console.log("message sent!! ", request)
+  // } catch (err) {
+  //   console.error("Unable to send message: ", err)
+  // }
 
   console.log("handle send api......")
 }
 
-const handleMessage = async (sender_psid, receive_message) => {
+const handleMessage = (sender_psid, receive_message) => {
   console.log("handle message......")
 
   let response
@@ -87,7 +108,7 @@ const handleMessage = async (sender_psid, receive_message) => {
     }
   }
 
-  await callSendAPI(sender_psid, response)
+  callSendAPI(sender_psid, response)
 }
 
 const handlePostBack = () => {
